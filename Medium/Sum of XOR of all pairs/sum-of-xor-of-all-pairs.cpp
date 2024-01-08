d5ca25a28d81ac50a7507dbe1e6856a6e535715f
Ganesh Yellapu <98867550+Ganeshyellapu@users.noreply.github.com>
2024-01-08 21:52:39 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Sum of XOR of all pairs/sum-of-xor-of-all-pairs.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+// An efficient C++ program to compute 
+// sum of bitwise OR of all pairs
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution{
+    public:
+    // Returns sum of bitwise OR
+    // of all pairs
+    long long int sumXOR(int arr[], int n)
+    {
+    	//Complete the function
+    	long long ans = 0;
+
+        for (int i = 0; i < 32; i++) {
+            long long z=0;
+            long long o=0;
+            for(int j=0;j<n;j++)
+            {
+                if(arr[j]&1){
+                    o+=1;
+                }
+                else{
+                    z+=1;
+                }
+                arr[j]=arr[j]>>1;
+            }
+            long long an=o*z*(1<<i);
+            ans+=an;
+        }
+
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	 int n ;
+	 cin>>n;
+	 int arr[n+1];
+	 for( int i=0;i<n;i++)
+	    cin>>arr[i];
+	 Solution ob;
+	 cout<<ob.sumXOR(arr, n)<<endl;
+	}	
+	return 0;
+}
+
+// } Driver Code Ends
