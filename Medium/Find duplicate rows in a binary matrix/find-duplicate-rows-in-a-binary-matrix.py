0698fae8017d2bac954e37f2c336fdeb78ea4e9b
Ganesh Yellapu <98867550+Ganeshyellapu@users.noreply.github.com>
2024-01-14 21:45:55 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Find duplicate rows in a binary matrix/find-duplicate-rows-in-a-binary-matrix.py
@@ -0,0 +1,44 @@
+#User function Template for python3
+
+class Solution:
+    def repeatedRows(self, arr, m ,n):
+        #code here
+        a=[]
+        for i in range(m-1):
+            for j in range(i+1,m):
+                if(arr[i]==arr[j]):
+                    a.append(j)
+        b=[]
+        a=sorted(a)
+        for i in a:
+            if i not in b:
+                b.append(i)
+        return b
+                
+                    
+                
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        n, m = list(map(int, input().strip().split()))
+        
+        arr = []
+        for i in range(n):
+            nums = list(map(int, input().strip().split()))
+            arr.append(nums)
+        ob = Solution()
+        ans = ob.repeatedRows(arr, n, m)
+        
+        for x in ans:
+            print(x, end=" ")
+            
+        print()
+        tc -= 1
+
+# } Driver Code Ends
